04df8300271d16d7b0c2afd7cc8f2e5008998650
NedaFahim <140852460+NedaFahim@users.noreply.github.com>
2024-10-14 01:11:36 +0330

    Added the search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "930bfof4e5b7tb729d2b7a0ceb8d91f3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let weatherAppCity = document.querySelector("#weather-app-city");
-  weatherAppCity.innerHTML = searchFormInput.value;
+
+  searchCity(searchFormInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Ahvaz");
