496b8c287b94f0d349793867a9f5c56676d64c64
NedaFahim <140852460+NedaFahim@users.noreply.github.com>
2024-10-14 02:14:18 +0330

    Added weather icon

--- src/script.js
+++ src/script.js
@@ -7,7 +7,9 @@ function refreshWeather(response) {
   let windSspeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"></img>`;
   timeElement.innerHTML = formatDate(date);
   windSspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
